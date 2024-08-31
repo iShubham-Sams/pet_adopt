07272f93e0c92df2a3f4330004a0a18fb160d21d
bhandari_shubham <shubhambhandari12345hb@gmail.com>
2024-08-31 14:06:04 +0530

    header create

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Header from "@/components/Home/Header";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home screen</Text>
+    <View style={{ padding: 20, marginTop: 20 }}>
+      {/* Header */}
+      <Header />
+      {/* Slider */}
+      {/* Category */}
+      {/* List Of Pets */}
+      {/* Add New Pet Option */}
     </View>
   );
 }
