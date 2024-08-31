eb6a1061052f063fb1a07ab1ee6707f6ea5d55dd
bhandari_shubham <shubhambhandari12345hb@gmail.com>
2024-08-31 12:26:54 +0530

    create app and tab navigation  add

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,16 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Colors from "@/constants/Colors";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+    <Tabs screenOptions={{ tabBarActiveTintColor: Colors.PRIMARY }}>
+      <Tabs.Screen name="home" options={{ title: "Home", headerShown: false, tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} /> }} />
+      <Tabs.Screen name="favorite" options={{ title: "Favorite", headerShown: false, tabBarIcon: ({ color }) => <Ionicons name="heart" size={24} color={color} /> }} />
+      <Tabs.Screen name="inbox" options={{ title: "Inbox", headerShown: false, tabBarIcon: ({ color }) => <Ionicons name="chatbox-ellipses" size={24} color={color} /> }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile", headerShown: false, tabBarIcon: ({ color }) => <Ionicons name="people-outline" size={24} color={color} /> }} />
     </Tabs>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { useUser } from "@clerk/clerk-expo";
+import { Redirect, useRootNavigationState } from "expo-router";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  const { user } = useUser();
+  const rootNavigationState = useRootNavigationState();
+  useEffect(() => {
+    CheckNavLoaded();
+  }, []);
+  const CheckNavLoaded = () => {
+    if (!rootNavigationState?.key) {
+      return null;
+    }
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <Text>{user?.fullName}</Text>
+      {user ? <Redirect href={"/(tabs)/home"} /> : <Redirect href="/login" />}
+    </View>
+  );
+}
