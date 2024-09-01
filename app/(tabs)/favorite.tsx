762be687731a8a592c825fb1d8f17ff0170d89fc
bhandari_shubham <shubhambhandari12345hb@gmail.com>
2024-09-01 18:05:42 +0530

    favorite page create

--- app/(tabs)/favorite.tsx
+++ app/(tabs)/favorite.tsx
@@ -1,10 +1,53 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useUser } from "@clerk/clerk-expo";
+import Shared from "@/Shared/Shared";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/config/FirebaseConfig";
+import PetListItem from "@/components/Home/PetListItem";
 
 export default function Favorite() {
+  const { user } = useUser();
+  const [faIds, setFavIds] = useState([]);
+  const [favPetList, setFavPetList] = useState<any[]>([]);
+  const [loaders, setLoaders] = useState(false);
+  useEffect(() => {
+    if (user) {
+      getFavPetIds();
+    }
+  }, [user]);
+  const getFavPetIds = async () => {
+    setLoaders(true);
+    const result = await Shared.getFavList(user);
+    setFavIds(result?.favorites);
+    setLoaders(false);
+    setFavPetList([]);
+    getFavPetList(result?.favorites);
+  };
+
+  const getFavPetList = async (favId: any) => {
+    setLoaders(true);
+    const q = query(collection(db, "Pets"), where("id", "in", favId));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      setFavPetList((pre) => [...pre, doc.data()]);
+    });
+    setLoaders(false);
+  };
   return (
-    <View>
-      <Text>Favorite</Text>
+    <View style={{ padding: 20, marginTop: 20 }}>
+      <Text style={{ fontFamily: "outfit-medium", fontSize: 30 }}>Favorites</Text>
+      <FlatList
+        data={favPetList}
+        numColumns={2}
+        onRefresh={getFavPetIds}
+        refreshing={loaders}
+        renderItem={({ item, index }) => (
+          <View>
+            <PetListItem pet={item} />
+          </View>
+        )}
+      />
     </View>
   );
 }
